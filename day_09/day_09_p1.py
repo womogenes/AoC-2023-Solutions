b6215bbe3c25e3c31860013a764293bd0ef3f219
William Y. Feng <womogenes2@gmail.com>
2023-12-08 22:14:33 -0800

    Finish day 9

---
+++ day_09/day_09_p1.py
@@ -0,0 +1,27 @@
+with open("./day_09.in") as fin:
+    lines = fin.read().strip().split("\n")
+
+
+def diff(arr):
+    return [arr[i+1] - arr[i] for i in range(len(arr) - 1)]
+
+
+def extrapolate(hist):
+    layers = [hist]
+
+    while not all([x == 0 for x in layers[-1]]):
+        layers.append(diff(layers[-1]))
+
+    layers[-1].append(0)
+    for i in range(len(layers) - 2, -1, -1):
+        layers[i].append(layers[i][-1] + layers[i+1][-1])
+
+    return layers[0][-1]
+
+
+ans = []
+for line in lines:
+    arr = list(map(int, line.split()))
+    ans.append(extrapolate(arr))
+
+print(sum(ans))
